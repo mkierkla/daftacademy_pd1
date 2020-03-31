cea59235a04a49f307c57e05a5f50aa6cf93f0c8
Maciej Kierkla <mkw234@gmail.com>
2020-03-31 16:31:20 +0200

    praca domowa

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get('/')
 def hello_world():
 	return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get('/method')
+def what_method(request: Request):
+	used_method = request.method
+	return {"method": used_method}
 
 @app.get('hello/{name}')
 def hello_name(name: str):
