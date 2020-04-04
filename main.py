55ab8f565e0d857e9bb4b4a26dc83ec38bf70ea1
Maciej Kierkla <mkw234@gmail.com>
2020-04-04 20:21:20 +0200

    praca domowa

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from starlette.routing import Route
 from pydantic import BaseModel
 
 app = FastAPI()
+app.counter = 0
 
+#zadanie1
 @app.get('/')
 def hello_world():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
+#zadanie2
 @app.get('/method')
 async def what_method(request: Request):
 	used_method = request.method
@@ -23,6 +26,24 @@ async def what_method(request: Request):
 	used_method = request.method
 	return {"method": used_method}
 
+#zadanie3
+
+class wez_pacjent(BaseModel):
+	imie: str
+	nazwisko: str
+
+class daj_pacjent(BaseModel):
+	id: int
+	pacjent: wez_pacjent
+
+@app.post('/patient', response_model=wez_pacjent)
+def wyswietl_pacjenta(rq: wez_pacjent):
+	gosciu = daj_pacjent(id=app.counter, pacjent=rq)
+	app.counter += 1
+	return gosciu
+
+
+#wyklad
 @app.get('hello/{name}')
 def hello_name(name: str):
 	return f"Hello {name}"
