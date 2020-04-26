ddbaab8f44f3ac30d5485de9287fdbf86fa691ad
Maciej Kierkla <mkw234@gmail.com>
2020-04-26 22:56:46 +0200

    praca domowa

--- main.py
+++ main.py
@@ -12,27 +12,37 @@ from hashlib import sha256
 app = FastAPI()
 app.secret_key = '432A462D4A614E645267556B58703273357538782F413F4428472B4B62502553'
 app.counter = 0
+app.sessions = []
 
 security = HTTPBasic()
 
-
-@app.post('/login')
-def logowanie(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
-	#sprawdzam czy dobre passy
-	correct_username = secrets.compare_digest(credentials.username, "trudnY")
-	correct_password = secrets.compare_digest(credentials.password, "PaC13Nt")
-	#komunikat jak zle
+def get_current_user(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+	user, passw = credentials.username, credentials.password
+	correct_username = secrets.compare_digest(user, "trudnY")
+	correct_password = secrets.compare_digest(password, "PaC13Nt")
 	if not (correct_username and correct_password):
 		raise HTTPException(
 			status_code=status.HTTP_401_UNAUTHORIZED,
 			detail="Incorrect email or password",
 			headers={"WWW-Authenticate": "Basic"},
 		)
-	#to bedzie sie dzialo jak dobre passy
-	else: 
-		session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}",encoding='utf8')).hexdigest()
-		response.set_cookie(key="session_token", value=session_token)
-		return RedirectResponse(url='/welcome') 
+	else:
+		session_token = sha256(bytes(f"{user}{password}{app.secret_key}", encoding = 'utf8')).hexdigest()
+		app.session_token.clear()
+		app.sessions.append(session_token)
+		return session_token 
+
+@app.post('/login')
+def login(response: Response, cookie: str = Depends(get_current_user)):
+    response.set_cookie(key = 'cookie', value = cookie)
+    return RedirectResponse(url='/welcome') 
+
+@app.post('/logout')
+def wylogowanie(response: Response):
+	session_token = app.sessions[0]
+	response.delete_cookie(key="session_token", value=session_token)
+	app.sessions.clear()
+	return RedirectResponse(url='/')
 
 @app.post('/welcome')
 @app.get('/welcome')
@@ -44,13 +54,13 @@ def powitanie():
 def hello_world():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
-#@app.post('welcome')
+@app.post('/method')
 @app.get('/method')
 async def what_method(request: Request):
 	used_method = request.method
 	return {"method": used_method}
 
-#@app.post('welcome')
+@app.post('/method')
 @app.put('/method')
 async def what_method(request: Request):
 	used_method = request.method
@@ -82,14 +92,14 @@ def wyswietl_pacjenta(rq: wez_pacjent):
 	return gosciu
 
 
-#@app.post('welcome')
+@app.post('/patient/{pk}')
 @app.get('/patient/{pk}')
 def znajdz_pacjetna(pk: int):
 	if pk not in [ziomek.id for ziomek in patients]:
 		return JSONResponse(status_code = 204, content ={})
 	return patients[pk].patient
 
-#@app.post('welcome')
+@app.post('hello/{name}')
 @app.get('hello/{name}')
 def hello_name(name: str):
 	return f"Hello {name}"
