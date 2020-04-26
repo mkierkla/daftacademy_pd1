0db30821f8b08996e8958799988a10ef03d51397
Maciej Kierkla <mkw234@gmail.com>
2020-04-26 22:28:41 +0200

    praca domowa

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+import secrets
+
+from fastapi import FastAPI, Request, Response, Depends, status, HTTPException, Cookie
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.responses import JSONResponse, RedirectResponse
 from starlette.routing import Route
 from pydantic import BaseModel
+from hashlib import sha256
+
+
 
 app = FastAPI()
+app.secret_key = '432A462D4A614E645267556B58703273357538782F413F4428472B4B62502553'
 app.counter = 0
 
-#zadanie1
+security = HTTPBasic()
+
+
+@app.post('/login')
+def logowanie(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
+	#sprawdzam czy dobre passy
+	correct_username = secrets.compare_digest(credentials.username, "gosc")
+	correct_password = secrets.compare_digest(credentials.password, "pass")
+	#komunikat jak zle
+	if not (correct_username and correct_password):
+		raise HTTPException(
+			status_code=status.HTTP_401_UNAUTHORIZED,
+			detail="Incorrect email or password",
+			headers={"WWW-Authenticate": "Basic"},
+		)
+	#to bedzie sie dzialo jak dobre passy
+	else: 
+		session_token = sha256(bytes(f"{credentials.username}{credentials.password}{app.secret_key}",encoding='utf8')).hexdigest()
+		response.set_cookie(key="session_token", value=session_token)
+		return RedirectResponse(url='/welcome') 
+
+@app.post('welcome')
+@app.get('/welcome')
+def hello_world():
+	return {"message": "jakis powitalny tekst"}
+
+#@app.post('welcome')
 @app.get('/')
 def hello_world():
 	return {"message": "Hello World during the coronavirus pandemic!"}
 
-#zadanie2
+#@app.post('welcome')
 @app.get('/method')
 async def what_method(request: Request):
 	used_method = request.method
 	return {"method": used_method}
 
+#@app.post('welcome')
 @app.put('/method')
 async def what_method(request: Request):
 	used_method = request.method
@@ -27,7 +61,7 @@ async def what_method(request: Request):
 	used_method = request.method
 	return {"method": used_method}
 
-#zadanie3
+
 global patients
 patients = []
 
@@ -39,6 +73,7 @@ class daj_pacjent(BaseModel):
 	id: int
 	patient: wez_pacjent
 
+#@app.post('welcome')
 @app.post('/patient', response_model=daj_pacjent)
 def wyswietl_pacjenta(rq: wez_pacjent):
 	gosciu = daj_pacjent(id=app.counter, patient=rq)
@@ -46,20 +81,21 @@ def wyswietl_pacjenta(rq: wez_pacjent):
 	app.counter += 1
 	return gosciu
 
-#zadanie 4
 
+#@app.post('welcome')
 @app.get('/patient/{pk}')
 def znajdz_pacjetna(pk: int):
 	if pk not in [ziomek.id for ziomek in patients]:
 		return JSONResponse(status_code = 204, content ={})
 	return patients[pk].patient
 
-#wyklad
+#@app.post('welcome')
 @app.get('hello/{name}')
 def hello_name(name: str):
 	return f"Hello {name}"
 	
-#pd3 zad1 
-@app.get('/welcome')
-def hello_world():
-	return {"message": "jakis powitalny tekst"}
+
+
+
+
+	
